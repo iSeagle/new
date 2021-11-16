e9e59678c7356aa256665452501d393700313bb5
deniz <denizkartal7@outlook.com>
2021-11-16 18:01:20 +0300

    aaa

---
+++ JS/index.js
@@ -0,0 +1,83 @@
+//console.log("external")
+
+// let selam = "helllo";
+// selam = "merhaba";
+// console.log(selam);
+
+/*var deger = "hellooo";
+console.log(deger);
+var deger = "merhaba";
+console.log(deger); */
+
+/*
+function fonn (){
+    var deger = "text";
+}
+fonn ();
+console.log(deger);
+*/
+
+//const text = "Javascript";
+//text = "hello";
+
+//datatype 
+// STRING TANIMLAMA çift tırnak ve tek tırnakla tanımlanabilir
+
+/*var ders = "ders";
+var ders = 'ders';
+*/
+//sayı 
+/*
+var sayi = 1;
+var ondalik = 1.15;
+//boolen
+var bool = true;
+*/
+// undefined 
+/*var a;
+console.log("deger"), typeof (a);
+var b = null;
+console.log("type"), typeof (b);
+*/
+
+// reference
+
+/*var lesson = {
+    name: 'web',
+    number: 1
+}
+console.log("object", lesson);
+console.log("object", lesson.name);
+*/
+//Array 
+/*
+var dizi = [1, 2, 3];
+console.log("dizi", typeof (dizi));
+console.log("item", dizi[1]);
+
+dizi[3] = 4;
+console.log(dizi);
+
+console.log(dizi.length);
+*/
+
+//function
+/*
+function hi(text){
+    console.log("function");
+}
+var text ="Text";
+hi(text);
+*/
+/*
+var num = "20c"; //notanumber
+var ynum = Number(num);
+
+console.log(ynum);
+*/
+
+/*
+var str = 1;
+var ss = String(str);
+console.log(ss)
+*/
