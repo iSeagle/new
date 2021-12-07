4dfd92c170a0a80bd5d98a18525d1674a5f95d45
deniz <denizkartal7@outlook.com>
2021-12-07 17:53:21 +0300

    aa

---
+++ JS/js 2. hafta/test.js
@@ -0,0 +1,26 @@
+/*let ders ="Web";
+if (ders=="Web") {
+       
+}
+else {
+
+}
+//switch 
+var gun= 2;
+switch (gun) {
+    case 1:
+        console.log("1. numaralı")
+        break;
+        case 2:
+            console.log("1. numaralı")
+
+    default:
+        break;
+}          */
+console.log("" - 5 + 1);
+console.log(null + 2);
+console.log("  -9  " + 2);
+console.log("4px" - 2);
+console.log(8 / "4");
+console.log(undefined + 2);
+
---
+++ JS/js 2. hafta/yas.js
@@ -0,0 +1,17 @@
+
+let dogum = prompt("Doğum yılını giriniz");
+
+var yas = 2021 - dogum;
+
+
+
+
+if (yas < 10) {
+    console.log("ilk okula gider")
+}
+else if (yas > 10 && yas < 20) {
+    console.log(" Üniversiteye gider")
+}
+else {
+    console.log("okul hayatı biter")
+}
