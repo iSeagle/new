3b6515ea1a4288782067b5fd74b6fedf7efd6762
deniz <denizkartal7@outlook.com>
2022-01-03 16:31:22 +0300

    covid

--- covidtest/covidt.js
+++ covidtest/covidt.js
@@ -1,7 +1,7 @@
 let covid = {
 
     fetchcovid: function (country) {
-        fetch("https://corona.lmao.ninja/v2/countries/" + country + "?yesterday&strict&query")
+        fetch("https://corona.lmao.ninja/v2/countries/" + country + "?yesterday=true&strict=true&query")
             .then((repsonse) => repsonse.json())
             .then((data) => this.displayCovid(data));
     },
@@ -11,11 +11,20 @@ let covid = {
         const { deaths } = data;
         const { todayDeaths } = data;
         const { todayCases } = data;
-        document.querySelector(".cases").innerText = "vaka sayısı: " + cases;
-        document.querySelector(".country").innerText = "ülke: " + country;
-        document.querySelector(".total-death").innerText = "Toplam Ölüm: " + deaths;
-        document.querySelector(".today-case").innerText = "günlük vaka: " + todayCases;
+
+
+        document.querySelector(".cases").innerText = " Toplam Vaka Sayısı: " + cases;
+        document.querySelector(".country").innerText = "Ülke Adı: " + country;
+        document.querySelector(".total-death").innerText = "Toplam Ölüm Sayısı: " + deaths;
+        document.querySelector(".today-case").innerText = "Günlük Vaka: " + todayCases;
         document.querySelector(".death").innerText = "Günlük Ölüm : " + todayDeaths;
+
+        if (todayCases == 0) {
+            document.querySelector(".today-case").innerText = "Günlük Vaka Sayısı :Henüz girilmedi";
+        }
+        if (todayDeaths == 0) {
+            document.querySelector(".death").innerText = "Günlük Ölüm Sayısı : Henüz girilmedi";
+        }
     },
     search: function () {
         this.fetchcovid(document.querySelector(".weather-search").value);
@@ -24,5 +33,12 @@ let covid = {
 document.querySelector(".search button").addEventListener("click", function () {
     covid.search();
 });
+document.querySelector(".search").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        covid.search();
+    }
+
+});
+covid.fetchcovid("Turkey");
 
 
